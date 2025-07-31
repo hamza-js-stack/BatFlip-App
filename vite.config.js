9641300005acc4cd84216cad9e5826e2f7f97652
hamza-js-stack <hamzamalik.99190@gmail.com>
2025-07-31 14:33:01 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/BatFlip-App/", // 👈 must match your repo name
   plugins: [react()],
-})
+});
